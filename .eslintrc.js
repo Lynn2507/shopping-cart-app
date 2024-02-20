c9b6a6f79d2204e8016058d37fa54fd389ae09d8
Lynn Yap <weilynny@gmail.com>
2024-02-20 12:05:24 +1030

    continuation

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,5 +8,10 @@ module.exports = {
 
     "env": {
         "es6": true
+    },
+    "parser": "babel-eslint",
+    "parserOptions": {
+        "sourceType": "module",
+        "allowImportExportEverywhere": true
     }
 };
--- app.js
+++ app.js
@@ -1,11 +1,35 @@
+// https://firebase.google.com/docs/database/web/start
+import  { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
-const inputField = document.querySelector("#input-field");
-const addBtn = document.getElementById("add-button");
+const appSettings = {
+    databaseURL: "https://shopping-cart-a8098-default-rtdb.asia-southeast1.firebasedatabase.app/"
+}
 
+// Initialize Firebase
+const app = initializeApp(appSettings)
+
+// Initialize Realtime Database and get a reference to the service
+const database = getDatabase(app)
+
+const shoppingListsInDb = ref(database, "shoppingLists")
+
+const inputField = document.querySelector("#input-field")
+const addBtn = document.getElementById("add-button")
+const shoppingListUI = document.getElementById("shopping-list")
 
 
 addBtn.addEventListener("click", function() {
-    console.log(inputField.value);
+    let item = inputField.value
+    addToShoppingList(item)
+    clear()
+    push(shoppingListsInDb, inputField.value)
+})
 
-});
+function clear() {
+    inputField.value = ""
+}
 
+function addToShoppingList(thingsToAdd) {
+    shoppingListUI.innerHTML += `<li>${thingsToAdd}</li>`
+}
